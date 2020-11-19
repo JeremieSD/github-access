855d93bf9b4550e8111ba9a5a13e3ff994208424
JeremieSD <jeremie9000@gmail.com>
2020-11-19 19:29:57 +0000

    changed it up, slightly more funcitonality for part 1.

--- collector.py
+++ collector.py
@@ -7,9 +7,30 @@ def print_user_info(gAPI):
     print("Location is: "+ gAPI.get_user().location)
 
 def main():
-    token = input("Enter Github token: ")
-    g = Github(token)
-    print_user_info(g)
+    ans = None
+    while (ans != 'y' and ans != 'n'):
+      ans = input("Do you want to login?:(y/n)")
+    if ans == 'y':
+      logged = False
+      while(not logged):
+        token = input("Enter Github token: ")
+        g = Github(token)
+        try:
+          g.get_user().login
+        except:
+          print("Authentication denied please try again.")
+        else:
+          print_user_info(g)
+          logged = True
+    else:
+      g = Github()
+      user = g.get_user("torvalds")
+      print ("Name is: " + user.login)
+      print("Full name is: " + user.name)
+      print("Location is: "+ user.location)
+      print(f"Public repos: {user.public_repos}")
+
+    
 
 if __name__ == "__main__":
     main()
